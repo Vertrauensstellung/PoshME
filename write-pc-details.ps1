04fdeccb2db1617b45b5a500b37f7ee0af1b961f
Vertrauensstellung <33381081+Vertrauensstellung@users.noreply.github.com>
2022-05-01 00:26:34 +0200

    New CVE Fixes

---
+++ write-pc-details.ps1
@@ -0,0 +1,14 @@
+﻿### Read out pc specs ###
+Write-Host PC-Info -ForegroundColor Green
+(Get-WmiObject -Class:Win32_ComputerSystem).Manufacturer
+(Get-WmiObject -Class:Win32_ComputerSystem).SystemFamily
+(Get-WmiObject -Class:Win32_ComputerSystem).Model
+(Get-WmiObject -Class:Win32_BIOS).SerialNumber
+Write-Host CPU -ForegroundColor Green
+(Get-WmiObject -Class Win32_Processor).Name
+(Get-WmiObject -Class Win32_Processor).NumberOfLogicalProcessors
+Write-Host Drive -ForegroundColor Green
+(Get-WmiObject -class win32_logicaldisk).Size / 1GB | % {$_.ToString("#.##")}
+(Get-WmiObject -class win32_logicaldisk).VolumeSerialNumber
+Write-Host RAM -ForegroundColor Green
+(Get-WmiObject -Class:Win32_ComputerSystem).TotalPhysicalMemory / 1GB | % {$_.ToString("#.##")}
